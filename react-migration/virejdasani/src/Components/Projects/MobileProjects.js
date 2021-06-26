3e784a50c1cfeb4def0cb442883c8fdc132a1f79
Virej Dasani <dasanivirej@gmail.com>
2021-06-26 22:35:24 +0530

    Add styling + project

--- react-migration/virejdasani/src/Components/Projects/MobileProjects.js
+++ react-migration/virejdasani/src/Components/Projects/MobileProjects.js
@@ -9,21 +9,30 @@ function MobileProjects() {
     <div id="mobileProjectsContainer">
       <HeadingTitle title="My Mobile Apps and Games" />
       <div className="scrolling-wrapper">
+        {/* Convert to everything */}
         <div id="card">
           <section className="card-row">
             <article className="card">
               <img
                 src={cteBanner}
-                alt="Convert To Everything"
+                alt="Convert To Everythinge fiwfiojwfojweoifjwoifoiwejfoijwefoijweoifjweoijfoiwejfoij"
                 height="125"
                 width="256"
               />
-              <h3>
-                <a href="">HII</a>
+              <h3 className="appName">
+                <a href="https://play.google.com/store/apps/details?id=com.virej.converttoeverything">
+                  Convert To Everything
+                </a>
               </h3>
-              <p>des</p>
+              <p>
+                Convert To Everything is the fastest, most practical unit
+                converter you've ever seen. This app removes the need of typing
+                what unit you want to convert to. Instead, it converts to all
+                available units! This makes it easier, faster and more practical
+                to use than any other!
+              </p>
               <a
-                href="https://play.google.com/store/apps/details?id=com.virej.ELECTRIFIED"
+                href="https://play.google.com/store/apps/details?id=com.virej.converttoeverything"
                 className="buttonLink"
               >
                 Get it on the Google PlayStore
