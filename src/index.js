aa6aa9f9b91aefca8ccaa43d4b44cdee3dc52d0b
Virej Dasani <dasanivirej@gmail.com>
2021-06-27 15:47:22 +0530

    New 404 page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./App";
+import NotFound from "./NotFound";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/virej" component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>,
   document.getElementById("root")
+  // </React.StrictMode>,
 );
