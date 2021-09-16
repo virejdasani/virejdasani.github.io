3437ff6f464d6fca38e27d68d020b2f9a2f2aeea
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-09-16 23:03:41 +0530

    Rm navbar alt text for image logos

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,75 +5,35 @@ function Navbar() {
     <div id="navbar">
       {/* HOME */}
       <a href="https://virejdasani.github.io">
-        <img
-          src={"img/homeLogo.png"}
-          alt="Virej Dasani"
-          height="22"
-          width="22"
-        />
+        <img src={"img/homeLogo.png"} alt="" height="22" width="22" />
       </a>
       {/* Instagram */}
       <a href="https://instagram.com/virejdasani">
-        <img
-          src={"img/instagramLogo.png"}
-          alt="Virej Dasani's Instagram"
-          height="22"
-          width="22"
-        />
+        <img src={"img/instagramLogo.png"} alt="" height="22" width="22" />
       </a>
       {/* Twitter */}
       <a href="https://twitter.com/virejdasani">
-        <img
-          src={"img/twitterLogo.png"}
-          alt="Virej Dasani's Twitter"
-          height="22"
-          width="22"
-        />
+        <img src={"img/twitterLogo.png"} alt="" height="22" width="22" />
       </a>
       {/* YouTube */}
       <a href="https://www.youtube.com/channel/UCRzS48bskynAxoI38BFypfQ">
-        <img
-          src={"img/youtubeLogo.png"}
-          alt="Virej Dasani's YouTube"
-          height="22"
-          width="27"
-        />
+        <img src={"img/youtubeLogo.png"} alt="" height="22" width="27" />
       </a>
       {/* Linkedin */}
       <a href="https://www.linkedin.com/in/virej-dasani-b64470209/">
-        <img
-          src={"img/linkedinLogo.png"}
-          alt="Virej Dasani's Linkedin"
-          height="22"
-          width="22"
-        />
+        <img src={"img/linkedinLogo.png"} alt="" height="22" width="22" />
       </a>
       {/* PlayStore */}
       <a href="https://play.google.com/store/apps/developer?id=Virej+Dasani">
-        <img
-          src={"img/playstoreLogo.png"}
-          alt="Virej Dasani's Play Store apps & games"
-          height="22"
-          width="18"
-        />
+        <img src={"img/playstoreLogo.png"} alt="" height="22" width="18" />
       </a>
       {/* Dev */}
       <a href="https://dev.to/virejdasani">
-        <img
-          src={"img/devLogo.svg"}
-          alt="Virej Dasani's Dev.to"
-          height="22"
-          width="18"
-        />
+        <img src={"img/devLogo.svg"} alt="" height="22" width="18" />
       </a>
       {/* GitHub */}
       <a href="https://github.com/virejdasani">
-        <img
-          src={"img/githubLogo.png"}
-          alt="Virej Dasani's GitHub Profile"
-          height="22"
-          width="22"
-        />
+        <img src={"img/githubLogo.png"} alt="" height="22" width="22" />
       </a>
     </div>
   );
