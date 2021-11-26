9e032c59a12dc1b71da57d40b3fd79d188a9063c
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-11-26 14:51:29 +0530

    New stuff

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -6,7 +6,7 @@ function Title() {
       <div id="titleHead">
         <h1>Virej Dasani</h1>
         <h3 id="tagline">
-          I'm a <span className="taglineHighlights">16 year old</span> student,{" "}
+          I'm a <span className="taglineHighlights">17 year old</span> student,{" "}
           and self-taught{" "}
           <span className="taglineHighlights">Web, App, & Game developer</span>
         </h3>
