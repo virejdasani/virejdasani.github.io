8845662bc73492454ec04fdd7f75b2257011c0c2
Virej Dasani <virej@Virejs-MacBook-Pro.local>
2022-10-10 10:40:53 +0100

    bg video optimization

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -5,6 +5,24 @@ function Title() {
     <div>
       <div id="titleHead">
         <h1>Virej Dasani</h1>
+        {/* <video playsinline="" autoplay="" muted="" loop="" class="hero__background-video hero__background-video--light">
+        <source src="https://themes.fourthwall.com/themes/assets/11d2f253-34b7-4fdc-b01b-17868cd74972/assets/video-bg-dark.mp4?v=364d6351-2cdd-8622-b962-d429a0989974" type="video/mp4">
+        Your browser does not support HTML5 video.
+      </video> */}
+        <video
+          className="video-bg"
+          autoPlay
+          loop
+          muted
+          poster="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
+        >
+          <source
+            src={
+              "https://github.com/virejdasani/virejdasani.github.io/blob/master/dots-moving-video-bg-dark.mp4?raw=true"
+            }
+            type="video/mp4"
+          />
+        </video>
         <h3 id="tagline">
           I code{" "}
           <span className="taglineHighlights">apps, games, websites</span> then{" "}
